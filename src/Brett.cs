3a5b3c32b9cecbbac1c7dc56d6f7155b7be02af4
Oyvind <oyvhov@gmail.com>
2010-02-10 12:06:36 +0100

    Har laga klasse for brett i 2D
    Signed-off-by: Oyvind <oyvhov@gmail.com>

---
+++ src/Brett.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameOfLife
+{   
+    //Klasse for 2D tabell
+    class Brett
+    {
+        
+    }
+}
