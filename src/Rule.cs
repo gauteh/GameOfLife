75f185d67a2b882e6bdacc5f24038fc4c36b9b0e
Trond <Felles@al@hotmail.com>
2010-03-15 22:55:30 +0100

    Ser ut til å funke på en, eller første gen.

--- src/Rule.cs
+++ src/Rule.cs
@@ -31,8 +31,8 @@ namespace GameOfLife
             // A og B er kordinatene i et grid.
             int A = 0; // A, rad. 0,0 - 0, 1 = a, 0 - a, 1
             int B = 0; // B, kolonne. 0,0 - 0 , 1 = 0, b - 0, b+1
-            int rad = 29;
-            int kol = 67;
+            int kol = Table.WIDTH-1;
+            int rad = Table.HEIGHT-1;
 
             int nabo = 0;
 
@@ -42,11 +42,12 @@ namespace GameOfLife
                 B = 0;
                 for (; B <= kol; B++)
                 {
-                    nabo = 0;
+                    
                 // forløkke skal gå igjennom alle kolonnene.
 
                     // Kode skal sjekke alle cellene rundt en celle med kord. A,B.
                     // Og telle opp hvor mange celler det er noe i.
+                    nabo = 0;
                     if (!((A+1) > rad || (A-1) < 0 || (B+1) > kol || (B-1) < 0))
                     {
                         
@@ -73,7 +74,7 @@ namespace GameOfLife
                     // Hvis det er noe i A,B så skal cellen dø om det er 0,1,4-8 celler rundt.
                     // og leve om det er 2,3.
                     // Hvis det er tom celle skal det bli ny celle ved at det er 3 naboer.
-
+                    
                     if (tables[0][A, B] != 0) // lever celle i A,B.
                     {
                         if (nabo == 2 || nabo == 3) // leve
@@ -86,7 +87,88 @@ namespace GameOfLife
                         if (nabo == 3)
                             tables[1][A, B] = 1;
                     }
+                    
+                    // klar for ny runde...
 
+                }
+            }
+
+        }
+
+        public void trondRegel(ref List<int[,]> tables)
+        {
+            // Forslag til regelfunksjon frå Trond
+            Console.WriteLine("[Rule] - ApplyRule");
+
+            // Test, fyller dei tre første rutene
+            /*
+            tables[1][0, 0] = 1;
+            tables[1][0, 1] = 1;
+            tables[1][0, 2] = 1;
+            */
+
+            // Har bytta ut alle tabOrg med tables[0] og alle tabNy med tables[1]
+            // tables er ein referanse til tabellane
+
+            // A og B er kordinatene i et grid.
+            int A = 0; // A, rad. 
+            int B = 0; // B, kolonne. 
+            int kol = Table.WIDTH - 1;
+            int rad = Table.HEIGHT - 1;
+
+            int nabo = 0;
+
+            for (; A <= rad; A++)
+            {
+                // forløkke skal gå igjennom alle radene.
+                B = 0;
+                for (; B <= kol; B++)
+                {                    
+                    // forløkke skal gå igjennom alle kolonnene.
+
+                    // Kode skal sjekke alle cellene rundt en celle med kord. A,B.
+                    // Og telle opp hvor mange celler det er noe i.
+                    nabo = 0;
+                    if (!((A + 1) > rad || (A - 1) < 0 || (B + 1) > kol || (B - 1) < 0))
+                    {
+
+                        if (tables[0][A - 1, B - 1] != 0)
+                            nabo++;
+                        if (tables[0][A - 1, B] != 0)
+                            nabo++;
+                        if (tables[0][A - 1, B + 1] != 0)
+                            nabo++;
+                        if (tables[0][A, B - 1] != 0)
+                            nabo++;
+                        if (tables[0][A, B + 1] != 0) // hopper jo over A,B.
+                            nabo++;
+                        if (tables[0][A + 1, B - 1] != 0)
+                            nabo++;
+                        if (tables[0][A + 1, B] != 0)
+                            nabo++;
+                        if (tables[0][A + 1, B + 1] != 0)
+                            nabo++;
+
+                    } // slutt if\else
+
+
+                    // Hvis det er noe i A,B så skal cellen dø om det er 0,1,4-8 celler rundt.
+                    // og leve om det er 2,3.
+                    // Hvis det er tom celle skal det bli ny celle ved at det er 3 naboer.
+
+                        
+                    if (tables[0][A, B] != 0) // lever celle i A,B.
+                    {
+                        if (nabo == 2 || nabo == 3) // leve
+                            tables[1][A, B] = 1;
+                        else                        // dø
+                            tables[1][A, B] = 0;
+                    }
+                    else
+                    {
+                        if (nabo == 3)
+                            tables[1][A, B] = 1;
+                    }
                     
 
                     // klar for ny runde...
@@ -95,6 +177,8 @@ namespace GameOfLife
             }
 
         }
+
+
     }
 }
 
